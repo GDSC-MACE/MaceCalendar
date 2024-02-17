8b5068fdb5223c5dbd99d48b00eeec0e201b7792
Krishnapriya-prasannan <krishnapriyaprasannan1@gmail.com>
2024-02-17 11:06:41 +0530

    some changes

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -1,30 +1,29 @@
-import './AboutUs.css';
-const AboutUs = () =>{
-return(
-    <div className="about">
-        <div className="photo">
-   <img src="https://tse3.mm.bing.net/th?id=OIP.h-g6x28CCi-SMzfGWdfzoQHaHa&pid=Api&P=0&h=180"></img>
-   <img src="https://tse3.mm.bing.net/th?id=OIP.QXkORNsTeQu-5EBDweBRSgHaHa&pid=Api&P=0&h=180"></img>
-   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/mar-1024.png"></img>
-   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/apr-1024.png"></img> 
-   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/may-1024.png"></img>
-   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/june-1024.png"></img>
-   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/july-1024.png"></img>
-   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/aug-1024.png"></img>
-   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/sept-1024.png"></img>
-   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/oct-1024.png"></img>
-   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/nov-1024.png"></img>
-   <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/dec-1024.png"></img>
-   
-        </div>
-    <div className="content">   
-  <h1>ABOUT US</h1>
-  
+import React from 'react';
 
-  <p>We GDSC MACE presents to you the Event Calender</p>
-  <p>Event Clender displays events hosted by all the clubs currently active in MACE  </p>
-  </div> 
-</div>);
+const AboutUs = () => {
+  return (
+    <div className="max-w-screen-lg mx-auto flex ml-5 w-full h-96">
+      <div className="flex-1 photo grid grid-cols-3 gap-4">
+        <img src="https://tse3.mm.bing.net/th?id=OIP.h-g6x28CCi-SMzfGWdfzoQHaHa&pid=Api&P=0&h=180" alt="img" className="pt-8 w-full h-full object-cover" />
+        <img src="https://tse3.mm.bing.net/th?id=OIP.QXkORNsTeQu-5EBDweBRSgHaHa&pid=Api&P=0&h=180" alt="img" className="pt-8 w-full h-full object-cover" />
+        <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/mar-1024.png" alt="img" className="pt-8 w-full h-full object-cover" />
+        <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/apr-1024.png" alt="img" className="pt-8 w-full h-full object-cover" />
+        <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/may-1024.png" alt="img" className="pt-8 w-full h-full object-cover" />
+        <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/june-1024.png" alt="img" className="pt-8 w-full h-full object-cover" />
+        <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/july-1024.png" alt="img" className="pt-8 w-full h-full object-cover" />
+        <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/aug-1024.png" alt="img" className="pt-8 w-full h-full object-cover" />
+        <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/sept-1024.png" alt="img" className="pt-8 w-full h-full object-cover" />
+        <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/oct-1024.png" alt="img" className="pt-8 w-full h-full object-cover" />
+        <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/nov-1024.png" alt="img" className="pt-8 w-full h-full object-cover" />
+        <img src="https://cdn0.iconfinder.com/data/icons/office-50/512/dec-1024.png" alt="img" className="pt-8 w-full h-full object-cover" />
+      </div>
+      <div className="flex-1 p-10">
+        <h1 className="text-3xl font-bold">ABOUT US</h1>
+        <p className="mt-4">We GDSC MACE presents to you the Event Calendar</p>
+        <p>Event Calendar displays events hosted by all the clubs currently active in MACE</p>
+      </div>
+    </div>
+  );
 };
 
 export default AboutUs;
