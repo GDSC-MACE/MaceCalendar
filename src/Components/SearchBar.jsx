3ba8a6a55094b136d874a5cbba14336deff8165a
Krishnapriya-prasannan <krishnapriyaprasannan1@gmail.com>
2024-02-19 10:31:35 +0530

    search-bar updated

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -4,7 +4,7 @@ import {AiOutlineSearch} from "react-icons/ai";
 
 export const SearchBar = () => {
     return(
-        <form className='w-[950px] relative'>
+        <form className='max-w-[950px] w-full mx-auto relative'>
             <div className="relative">
                 <input type="search" placeholder='Type Here' className="w-full p-4 rounded-full border border-solid border-slate-300 bg-white   " />
                 <button className="absolute right-1 top-1/2 -translate-y-1/2 p-4 bg-slate-400 rounded-full">
